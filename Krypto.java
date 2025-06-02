3866c602801e383b833f38ea92e0a7b0c7d43871
lunat <lukiloewe@aol.com>
2025-06-02 16:50:12 +0200

    Ceasar cipher (lower case)

--- Krypto.java
+++ Krypto.java
@@ -16,10 +16,27 @@ public class Krypto {
         return letterCount;
     }
 
+    public String caesar(String text, int n) {
+        text = text.toLowerCase();
+        StringBuilder res = new StringBuilder();
+        for (int i = 0; i < text.length(); i++) {
+            if (text.charAt(i) == ' ') {
+                res.append(" ");
+            } else {
+                int letterCode = ((int) text.charAt(i) - 'a' + n) % 26;
+                res.append((char) (letterCode + 'a'));
+            }
+        }
+        return res.toString();
+    }
+
     public static void main(String[] args) {
         Krypto krypto = new Krypto();
         String text = "Bailey Whitfield 'Whit' Diffie ForMemRS (born June 5, 1944) is an American cryptographer and mathematician and one of the pioneers of public-key cryptography along with Martin Hellman and Ralph Merkle. Diffie and Hellman's 1976 paper New Directions in Cryptography[5] introduced a radically new method of distributing cryptographic keys, that helped solve key distribution—a fundamental problem in cryptography. Their technique became known as Diffie–Hellman key exchange. The article stimulated the almost immediate public development of a new class of encryption algorithms, the asymmetric key algorithms.";
         String example = "Hallo Moin";
-        System.out.println(krypto.letterCount(example));
+        String exampleEncrypt = krypto.caesar(example, 5);
+        System.out.println(exampleEncrypt);
+        String exampleDecrypt = krypto.caesar(exampleEncrypt, -5);
+        System.out.println(exampleDecrypt);
     }
 }
