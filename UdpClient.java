4f6322039dc88560ffd58ab718ee9f01890fb3ca
lunat <lukiloewe@aol.com>
2025-06-03 10:09:11 +0200

    Udp Server Encrypt and Client

---
+++ UdpClient.java
@@ -0,0 +1,32 @@
+import java.net.*;
+
+public class UdpClient {
+    DatagramSocket socket;
+    InetAddress address;
+
+    byte[] buffer = new byte[1024];
+
+    public UdpClient() throws UnknownHostException, SocketException {
+        socket = new DatagramSocket();
+        address = InetAddress.getByName("localhost");
+    }
+
+    public String send(String message) throws Exception {
+        buffer = message.getBytes();
+        DatagramPacket packet = new DatagramPacket(buffer, buffer.length, address, 8888);
+        socket.send(packet);
+        DatagramPacket response = new DatagramPacket(buffer, buffer.length);
+        socket.receive(response);
+        return new String(response.getData(), 0, response.getLength());
+    }
+    public void close() {
+        socket.close();
+    }
+    public static void main(String[] args) throws Exception {
+        UdpClient client = new UdpClient();
+        String response = client.send("Hallo Moin");
+        System.out.println(response);
+        client.close();
+    }
+
+}
---
+++ UdpServerEncrypt.java
@@ -0,0 +1,45 @@
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.net.SocketException;
+
+public class UdpServerEncrypt extends Thread{
+    DatagramSocket socket;
+    byte[] buffer = new byte[1024];
+
+    public UdpServerEncrypt() throws SocketException {
+        socket = new DatagramSocket(8888);
+    }
+
+    @Override
+    public void run() {
+        while(true) {
+            DatagramPacket packet = new DatagramPacket(buffer, buffer.length);
+            try {
+                socket.receive(packet);
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+            InetAddress address = packet.getAddress();
+            int port = packet.getPort();
+
+            DatagramPacket response = new DatagramPacket(buffer, buffer.length, address, port);
+            String received = new String(packet.getData(), 0, packet.getLength());
+            Krypto krypto = new Krypto();
+            krypto.caesar(received, 5);
+            response.setData(krypto.caesar(received, 5).getBytes());
+            try {
+                socket.send(response);
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+            socket.close();
+        }
+    }
+
+    public static void main(String[] args) throws SocketException {
+        UdpServerEncrypt server = new UdpServerEncrypt();
+        server.start();
+    }
+}
