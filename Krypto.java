92f50d39f85b6fdb0f81a8f944f5526e3dbe5197
lunat <lukiloewe@aol.com>
2025-06-06 19:05:35 +0200

    Ceaear refactoring

--- Krypto.java
+++ Krypto.java
@@ -17,24 +17,32 @@ public class Krypto {
     }
 
     public String caesar(String text, int n) {
-        text = text.toLowerCase();
+        char[] arr = text.toCharArray();
         StringBuilder res = new StringBuilder();
-        for (int i = 0; i < text.length(); i++) {
-            if (text.charAt(i) == ' ') {
-                res.append(" ");
-            } else {
-                int letterCode = ((int) text.charAt(i) - 'a' + n) % 26;
-                res.append((char) (letterCode + 'a'));
+        for (char c : arr) {
+            if (Character.isUpperCase(c)) {
+                int offset = (c - 'A' + n) % 26;
+                if (offset < 0) offset += 26;
+                res.append((char) ('A' + offset));
+            } else if (Character.isLowerCase(c)) {
+                int offset = (c - 'a' + n) % 26;
+                if (offset < 0) offset += 26;
+                res.append((char) ('a' + offset));
+            }
+            else {
+                res.append(c);
             }
         }
         return res.toString();
     }
 
+    //public String viginere(String text, int n) {}
+
     public static void main(String[] args) {
         Krypto krypto = new Krypto();
         String text = "Bailey Whitfield 'Whit' Diffie ForMemRS (born June 5, 1944) is an American cryptographer and mathematician and one of the pioneers of public-key cryptography along with Martin Hellman and Ralph Merkle. Diffie and Hellman's 1976 paper New Directions in Cryptography[5] introduced a radically new method of distributing cryptographic keys, that helped solve key distribution—a fundamental problem in cryptography. Their technique became known as Diffie–Hellman key exchange. The article stimulated the almost immediate public development of a new class of encryption algorithms, the asymmetric key algorithms.";
         String example = "Hallo Moin";
-        String exampleEncrypt = krypto.caesar(example, 5);
+        String exampleEncrypt = krypto.caesar(text, 5);
         System.out.println(exampleEncrypt);
         String exampleDecrypt = krypto.caesar(exampleEncrypt, -5);
         System.out.println(exampleDecrypt);
